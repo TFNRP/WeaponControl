1eef282ed1836caaf63fb48fcdf4140ad3ae99be
Hagen <hagen@paw.bot>
2021-11-28 15:50:58 +0000

    chore: add repository information

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
+repository 'https://github.com/TFNRP/WeaponControl'
+version '0.1.0'
+author 'Reece Stokes <hagen@hyena.gay>'
+
 client_script {
   'util.lua',
   'client.lua',
